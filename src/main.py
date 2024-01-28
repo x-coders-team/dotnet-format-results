974237644830846a36abf8d76be3778b18f6ccba
Rafał Salamon <rasa@salamonrafal.pl>
2024-01-28 13:52:16 +0100

    PoC version #3

--- src/main.py
+++ src/main.py
@@ -1,2 +1,2 @@
-html_output = "<html><head><title>Testing</title></head><body><p>Testing workflow</p></body></html>"
+html_output = "<html><head><title>Testing from Python</title></head><body><p>Testing workflow</p></body></html>"
 print(html_output)
