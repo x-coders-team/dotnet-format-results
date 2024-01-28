126c93a88e43a6a80bc4cfa13baaffda1cc88350
Rafał Salamon <rasa@salamonrafal.pl>
2024-01-28 13:59:28 +0100

    PoC version #3

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -1,7 +1,7 @@
 #!/bin/sh -l
 
 echo "JSON Input $1"
-if html_output=$(python ./src/main.py); then
+if html_output=$(python ./src/main.py $1); then
     echo "html-output=$html_output" >> $GITHUB_OUTPUT
 else
     echo "Unable return HTML output"
--- src/main.py
+++ src/main.py
@@ -1,2 +1,4 @@
-html_output = "<html><head><title>Testing from Python</title></head><body><p>Testing workflow</p></body></html>"
+import sys
+json_input = sys.argv[1]
+html_output = f"<html><head><title>Testing from Python</title></head><body><p>Testing workflow</p><div>{json_input}</div></body></html>"
 print(html_output)
