975c97fe81261e0e7b3bc320a92995cb371ca4c4
Rafał Salamon <rasa@salamonrafal.pl>
2024-02-03 13:25:00 +0100

    Poc v1

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -4,8 +4,9 @@ echo "JSON Input $1"
 echo "Runner work directory $2"
 
 ls -l $GITHUB_WORKSPACE
+ls -l /action
 
-if html_output=$(python ./src/main.py "$1" "$2"); then
+if html_output=$(python /action/src/main.py "$1" "$2"); then
     echo "html-output=$html_output" >> $GITHUB_OUTPUT
 else
     echo "Unable return HTML output"
