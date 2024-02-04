c356e9cb4ae4a1735ea0bdb118d9c53092c0fcaa
Rafał Salamon <rasa@salamonrafal.pl>
2024-02-04 14:28:53 +0100

    Poc v1

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -9,7 +9,13 @@ else
     echo "[KO] pip install githubkit"
 fi
 
-echo -e "${GITHUB_SHA} \n"
+echo  "GITHUB_SHA: ${GITHUB_SHA} \n"
+echo  "GITHUB_REPOSITORY_OWNER: ${GITHUB_REPOSITORY_OWNER} \n"
+echo  "GITHUB_REPOSITORY: ${GITHUB_REPOSITORY} \n"
+echo  "GITHUB_HEAD_REF: ${GITHUB_HEAD_REF} \n"
+echo  "GITHUB_ACTION_REF: ${GITHUB_ACTION_REF} \n"
+
+
 
 if html_output=$(python /action/src/main.py "$1" "$2"); then
     echo "html-output=$html_output" >> $GITHUB_OUTPUT
